ec78b61b620961e745854efe267cda79370baa23
yousuf451 <yousufshaikh92@gmail.com>
2018-08-05 15:41:11 +0500

    callback practices

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("Yousuf Shaikh");
+// function doSomeThing(data){
+//     console.log(data);
+// }
+
+// var a  = doSomeThing("Hello Test");
+// console.log(a);
+
+
+/// Hoisting /// 
+
+// console.log(a);
+// var a = "Test hoisting"; 
+
+function doSomeWork(callback) {
+    // working ... async is going in this line 
+    callback("Hello World");
+}
+
+var a = doSomeWork(function(data){
+    console.log(data);
+})
+
+console.log("after function");
